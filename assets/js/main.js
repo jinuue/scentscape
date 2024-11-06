5862bc2d3c2059d4d924292399da2af3dbae9ae0
Princess Arielle Tungpalan <123855325+lyxgm@users.noreply.github.com>
2024-11-06 12:28:39 +0000

    oks guide, edit nlng

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,6 +91,27 @@ let swiperProducts = new Swiper(".new__container", {
   },
 });
 
+/*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabsContents = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabsContents.forEach((tabsContent) => {
+      tabsContent.classList.remove("active-tab");
+    });
+
+    target.classList.add("active-tab");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+
+    tab.classList.add("active-tab");
+  });
+});
 // Define product data
 const products = [
   {
